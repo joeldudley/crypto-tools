ca53a95bb4bb07456ddb9ebae0a430cca5a70d98
joeldudley <joel@obscu.ro>
2022-12-04 21:02:00 +0000

    Returns Vec<u8> not string from XOR crackers.

--- src/crackers/xor_ciphers.rs
+++ src/crackers/xor_ciphers.rs
@@ -2,26 +2,34 @@ use crate::bitflips::xor::*;
 use crate::scorers::english_scorers::*;
 use crate::scorers::hamming_distance::hamming_distance;
 
+#[derive(Debug)]
+pub struct EmptyArrayError;
+
 /// Returns the plaintext encoded using a single-byte XOR cipher. Works by selecting the XOR key
 /// that results in the most "english-like" plaintext.
-// TODO - Should return &[u8].
-pub fn crack_single_byte_xor_cipher(ciphertext: &[u8]) -> String {
-    let plaintext = (0u8..255)
+pub fn crack_single_byte_xor_cipher(ciphertext: &[u8]) -> Vec<u8> {
+    (0u8..255)
         .map(|x| xor(ciphertext, &x))
         .max_by(|x, y| english_score(x).total_cmp(&english_score(y)))
-        .expect("we know a maximum will be found");
-
-    return String::from_utf8_lossy(plaintext.as_slice()).to_string();
+        .expect("we know a maximum will be found")
+        .to_vec()
 }
 
 /// Returns the plaintext encoded using a single-byte XOR cipher among a list of possible
 /// ciphertexts.
-// TODO - Should return Option<&[u8]>.
-pub fn detect_and_crack_single_byte_xor_cipher(possible_ciphertexts: &[&[u8]]) -> Option<String> {
-    return possible_ciphertexts
+pub fn detect_and_crack_single_byte_xor_cipher(possible_ciphertexts: &[&[u8]]) -> Result<Vec<u8>, EmptyArrayError> {
+    if possible_ciphertexts.is_empty() {
+        return Err(EmptyArrayError);
+    }
+
+    let plaintext = possible_ciphertexts
         .iter()
         .map(|x| crack_single_byte_xor_cipher(x))
-        .max_by(|x, y| english_score(x.as_bytes()).total_cmp(&english_score(y.as_bytes())));
+        .max_by(|x, y| english_score(x).total_cmp(&english_score(y)))
+        .expect("we know a maximum will be found")
+        .to_vec();
+
+    Ok(plaintext)
 }
 
 /// Finds the key size (of between 2 and 40 bytes) used to encrypt a repeating XOR cipher.
@@ -65,7 +73,7 @@ mod tests {
     #[test]
     fn can_crack_single_byte_xor_cipher() {
         let ciphertext = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-        let expected_plaintext = "Cooking MC's like a pound of bacon";
+        let expected_plaintext = "Cooking MC's like a pound of bacon".as_bytes();
 
         let ciphertext_bytes = hex::decode(ciphertext).expect("could not convert hex to bytes");
         let plaintext = crack_single_byte_xor_cipher(&ciphertext_bytes);
@@ -83,7 +91,7 @@ mod tests {
                 .expect("could not convert hex to bytes"))
             .collect::<Vec<Vec<u8>>>();
         let ciphertexts_bytes = ciphertexts_bytes_vec.iter().map(|x| &x[..]).collect::<Vec<&[u8]>>();
-        let expected_plaintext = "Now that the party is jumping\n";
+        let expected_plaintext = "Now that the party is jumping\n".as_bytes();
 
         let plaintext = detect_and_crack_single_byte_xor_cipher(&ciphertexts_bytes)
             .expect("could not find plaintext");
